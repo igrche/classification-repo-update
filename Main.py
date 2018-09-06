e6843193c368650234fda9ffdc0d3d5891a0240e
Igor Chebykin <ichebyki@gmail.com>
2018-09-06 20:40:55 +0700

    update

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import shutil
 import hashlib
 import logging
 import sys
+from shutil import *
 from ftpreconnect.ftpReconnect import PyFTPclient as FtpReconnect
 from ftpmultipart.ftpMultipart import go as FtpMultipart
 
@@ -11,11 +12,44 @@ def withMultipart(grabber, host, port, login, password, cwd, file):
     FtpMultipart(grabber, host, port, login, password, cwd, file)
 
 
-def withReconnect(host, port, login, password, cwd, file):
+def withReconnect(host, port, login, password, cwd, FTP_file):
     obj = FtpReconnect(host, port, login, password)
     obj.setCwd(cwd)
     obj.setBlockSize(8192 * 32)
-    obj.DownloadFile(file)
+    obj.setFileName(FTP_file)
+
+    filesize = obj.getFileSize()
+
+    FTP_parts = 8
+    chunk_size = filesize / FTP_parts
+    last_chunk_size = filesize - (chunk_size * (FTP_parts - 1))
+
+    downloaders = []
+    for i in range(FTP_parts):
+        if i == (FTP_parts - 1):
+            this_chunk_size = last_chunk_size
+        else:
+            this_chunk_size = chunk_size
+
+        obj = FtpReconnect(host, port, login, password)
+        obj.setCwd(cwd)
+        obj.setBlockSize(8192 * 32)
+        obj.setFileName(FTP_file)
+        obj.setLocalName(FTP_file + ".part-" + str(i))
+        obj.setChunkStart(chunk_size * i)
+        obj.setChunkSize(this_chunk_size)
+        obj.downloadFileInThread()
+
+        downloaders.append(obj)
+
+    for downloader in downloaders:
+        downloader.thread.join()
+
+    with open(FTP_file, 'w+b') as f:
+        for downloader in downloaders:
+            copyfileobj(open(downloader.local_filename, 'rb'), f)
+
+    return 1
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
--- ftpreconnect/ftpReconnect.py
+++ ftpreconnect/ftpReconnect.py
@@ -8,6 +8,7 @@ import socket
 import time
 import sys
 import os
+from threading import *
 
 
 def setInterval(interval, times = -1):
@@ -41,6 +42,9 @@ class Done(Exception):
 
 
 class PyFTPclient:
+
+    thread_number = 0
+
     def __init__(self, host, port, login, passwd, monitor_interval = 30):
         self.host = host
         self.port = port
@@ -55,7 +59,21 @@ class PyFTPclient:
         self.f = None
         self.local_filename = None
         self.block_size = 8192
+        self.fileName = None
+        self.chunkStart = 0
+        self.chunkSize = -1
+        self.thread_number = None
+        self.thread = None
 
+
+    def setLocalName(self, local_file_name):
+        self.local_filename = local_file_name
+
+    def setChunkStart(self, param):
+        self.chunkStart = param
+
+    def setChunkSize(self, param):
+        self.chunkSize = param
 
     def setBlockSize(self, size=8192):
         self.block_size = size
@@ -63,17 +81,34 @@ class PyFTPclient:
     def setCwd(self, dir="/"):
         self.cwd = dir
 
+
+    def setFileName(self, file):
+        self.fileName = file
+
     def on_data(self, data):
         self.f.write(data)
-        if os.path.getsize(self.local_filename) >= self.dst_filesize:
+        if self.f.tell() >= self.dst_filesize:
             raise Done
 
-    def DownloadFile(self, dst_filename, local_filename = None):
+    def downloadFileInThread(self):
+        PyFTPclient.thread_number += 1
+        self.thread_number = PyFTPclient.thread_number
+        self.thread = Thread(target=self.downloadFile)
+        self.thread.start()
+
+    def connect(self, ftp):
+        ftp.connect(self.host, self.port)
+        ftp.login(self.login, self.passwd)
+        # optimize socket params for download task
+        ftp.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        # ftp.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 75)
+        # ftp.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 60)
+        ftp.cwd(self.cwd)
+
+    def downloadFile(self):
         res = ''
-        if local_filename is None:
-            self.local_filename = dst_filename
-        else:
-            self.local_filename = local_filename
+        if self.local_filename is None:
+            self.local_filename = self.fileName
 
         with open(self.local_filename, 'w+b') as f:
             self.f = f
@@ -90,32 +125,26 @@ class PyFTPclient:
                         ftp.close()
 
 
-            def connect():
-                ftp.connect(self.host, self.port)
-                ftp.login(self.login, self.passwd)
-                # optimize socket params for download task
-                ftp.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-                # ftp.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 75)
-                # ftp.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 60)
-                ftp.cwd(self.cwd)
-
 
             ftp = ftplib.FTP()
             ftp.set_debuglevel(2)
             ftp.set_pasv(True)
 
-            connect()
+            self.connect(ftp)
             ftp.voidcmd('TYPE I')
-            self.dst_filesize = ftp.size(dst_filename)
+            if self.chunkSize == -1:
+                self.dst_filesize = ftp.size(self.fileName)
+            else:
+                self.dst_filesize = self.chunkSize
 
             mon = monitor()
             while self.dst_filesize > self.f.tell():
                 try:
-                    connect()
+                    self.connect(ftp)
                     self.waiting = False
                     # retrieve file from position where we were disconnected
-                    res = ftp.retrbinary('RETR %s' % dst_filename, self.on_data, blocksize=self.block_size) if self.f.tell() == 0 \
-                        else ftp.retrbinary('RETR %s' % dst_filename, self.on_data, blocksize=self.block_size, rest=self.f.tell())
+                    res = ftp.retrbinary('RETR %s' % self.fileName, self.on_data, blocksize=self.block_size, rest=self.chunkStart) if self.f.tell() == 0 \
+                        else ftp.retrbinary('RETR %s' % self.fileName, self.on_data, blocksize=self.block_size, rest=self.chunkStart + self.f.tell())
 
                 except Done:
                     break
@@ -137,14 +166,31 @@ class PyFTPclient:
             ftp.close()
 
             if not res.startswith('226 Transfer complete'):
-                logging.error('Downloaded file {0} is not full.'.format(dst_filename))
-                # os.remove(self.local_filename)
+                if not self.f.tell() >= self.dst_filesize:
+                    logging.error('Downloaded file {0} is not full.'.format(self.fileName))
+                    # os.remove(self.local_filename)
+                    return None
+
+            if self.f.tell() > self.dst_filesize:
+                logging.info('Downloaded file size of {0} is {1}.'.format(self.fileName, self.f.tell()))
+                self.f.truncate(self.dst_filesize)
+                self.f.close()
+                logging.info('After trancating the file size is {0}.'.format(os.path.getsize(self.local_filename)))
+
+                return 1
+            else:
                 return None
 
+    def getFileSize(self):
+        ftp = ftplib.FTP()
+        ftp.set_debuglevel(2)
+        ftp.set_pasv(True)
 
-
-            return 1
-
+        self.connect(ftp)
+        ftp.voidcmd('TYPE I')
+        filesize = ftp.size(self.fileName)
+        ftp.quit()
+        return filesize
 
 
 if __name__ == "__main__":
