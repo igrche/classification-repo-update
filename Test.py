abc9f78ad001dd9119d6dc9508637baf9f115e6d
Igor Chebykin <ichebykin@unipro.ru>
2018-09-11 10:59:59 +0700

    add platform

--- Test.py
+++ Test.py
@@ -1,13 +1,20 @@
 import logging
 import sys
+import platform
 from DownloadAny import downloadURL
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
 
+    dest_dir = ''
+    if platform.system() == "Windows":
+        dest_dir = "e:\\ichebyki\\Downloads\\"
+    else:
+        dest_dir = "/home/ichebyki/Downloads"
+
     downloadURL('ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/uniref/uniref50/uniref50.fasta.gz',
-                "e:\\ichebyki\\Downloads\\")
+                dest_dir)
     downloadURL('ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/gi_taxid_nucl.zip',
-                "e:\\ichebyki\\Downloads\\")
+                dest_dir)
     downloadURL('http://releases.ubuntu.com/16.04/ubuntu-16.04.5-desktop-amd64.iso',
-                "e:\\ichebyki\\Downloads\\")
+                dest_dir)
